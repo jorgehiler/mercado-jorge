0c7723b2c6f95da7209f1c838fd9118347983e86
Jorge <andreshiler@gmail.com>
2020-02-12 13:12:14 -0500

    back to first page

--- src/app/grid-products/grid-products.component.ts
+++ src/app/grid-products/grid-products.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, OnChanges, DoCheck, SimpleChanges } from '@angular/core';
-import { PageEvent, MatPaginatorIntl } from '@angular/material';
+import { Component, OnInit, Input, OnChanges, DoCheck, SimpleChanges, ViewChild } from '@angular/core';
+import { PageEvent, MatPaginatorIntl, MatPaginator } from '@angular/material';
 import { Article } from '../article';
 import { DataService } from '../data.service';
 import { Observable, Subscription } from 'rxjs';
@@ -11,29 +11,23 @@ import { Observable, Subscription } from 'rxjs';
 })
 export class GridProductsComponent implements OnInit, OnChanges{
 
-  ngOnChanges(changes: SimpleChanges) {
-    if(changes.listArticles){
-      console.log("qui fue")
-      this.pagedList = this.listArticles.slice(0, this.pageSize);
-
-    }
-    // You can also use categoryId.previousValue and
-    // categoryId.firstChange for comparing old and new values
-  }
-
-  // MatPaginator Inputs
   length = 50;
   pageSize = 6;
   pageSizeOptions: number[] = [3, 6, 9, 12];
   @Input() listArticles: any[];
   pagedList: Article[];
   cols: number;
+  @ViewChild('paginator', {static: false}) paginator: MatPaginator;
 
-  @Input() events: Observable<void>;
-  eventsSubscription: Subscription;
-
-  // MatPaginator Output
   pageEvent: PageEvent;
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.listArticles){
+      this.pagedList = this.listArticles.slice(0, this.pageSize);
+      this.paginator.pageIndex = 0;
+    }
+  }
 
   setPageSizeOptions(setPageSizeOptionsInput: string) {
     if (setPageSizeOptionsInput) {
@@ -44,24 +38,12 @@ export class GridProductsComponent implements OnInit, OnChanges{
   constructor(private searchService: DataService) {
     this.listArticles = [];
     console.log(this.listArticles);
-    // this.length = this.listArticles.length;
-    // this.getSearch('gafas');
     this.cols = 4;
     this.listArticles = [];
   }
 
   ngOnInit() {
     this.listArticles = [];
-    // this.pagedList = this.listArticles.slice(0, this.pageSize);
-    this.eventsSubscription = this.events.subscribe(() => {
-      console.log("nueva bussqueda");
-      this.pagedList = this.listArticles.slice(0, this.pageSize);
-      console.log(this.listArticles);
-    });
-  }
-
-  ngOnDestroy() {
-    this.eventsSubscription.unsubscribe();
   }
 
   OnPageChange(event: PageEvent) {
@@ -76,15 +58,5 @@ export class GridProductsComponent implements OnInit, OnChanges{
     console.log("page size" + this.pageSize)
   }
 
-  // getSearch(txtSearch: string){
-  //   this.searchService.getSearch(txtSearch)
-  //   .subscribe(getSearch
-  //     res => { console.log(res);
-  //              this.resultSearch = res;
-  //              this.listArticles = this.resultSearch.results;
-  //             },
-  //     err => console.log('error' + err)
-  //   )
-  // }
 
 }
